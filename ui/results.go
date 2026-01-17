2571c49d05c941478ee3b3a190a905dc8c212e17
Christian Bjørnsrud <ckb78@outlook.com>
2026-01-17 16:44:30 +0100

    Refactor output

--- ui/results.go
+++ ui/results.go
@@ -6,18 +6,21 @@ import (
 	"github.com/Bjornsrud/ByteBench87/bench"
 )
 
+const seperator = "----------------------------"
+
 func PrintRunHeader(cfg bench.Config) {
 	fmt.Println("Starting benchmarks...")
 	fmt.Printf("Config:   warmup=%d runs=%d\n\n", cfg.WarmupRuns, cfg.Runs)
 }
 
 func PrintTestHeader(name string) {
-	fmt.Printf("Running %s...\n", name)
+	fmt.Printf("\nRunning %s...\n", name)
 }
 
 func PrintTestResult(r bench.Result) {
-	fmt.Println("*** Summary ***")
+	fmt.Printf(seperator + "\n")
 	fmt.Printf("Test:     %s\n", r.Name)
 	fmt.Printf("Median:   %v\n", r.Median)
-	fmt.Printf("Checksum: 0x%016x\n\n", r.Checksum)
+	fmt.Printf("Checksum: 0x%016x\n", r.Checksum)
+	fmt.Printf(seperator + "\n")
 }
